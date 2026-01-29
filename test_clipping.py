53de7e80d86683bbf04a4e15fc393ebdca466bd7
Matheus Freitas <f.matheus.fernandes.f@gmail.com>
2026-01-29 18:03:51 -0300

    feat: remove test script for clipping bounds

--- test_clipping.py
+++
@@ -1,6 +0,0 @@
-# Test script to verify clipping bounds
-minimap_bounds = (10, 10, 160, 120)
-xmin, ymin, xmax, ymax = minimap_bounds[0], minimap_bounds[1], minimap_bounds[0] + minimap_bounds[2], minimap_bounds[1] + minimap_bounds[3]
-
-print(f"Minimap bounds: {minimap_bounds}")
-print(f"xmin={xmin}, ymin={ymin}, xmax={xmax}, ymax={ymax}")
